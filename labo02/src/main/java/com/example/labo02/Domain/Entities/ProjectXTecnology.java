504c913e8092cfb375c8ad79a283a70b246619ab
diegohdez33 <00007222@uca.edu.sv>
2025-05-08 00:21:28 -0600

    Add: Project X Tecnology Entity

---
+++ labo02/src/main/java/com/example/labo02/Domain/Entities/ProjectXTecnology.java
@@ -0,0 +1,26 @@
+package com.example.labo02.Domain.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "project_x_tecnology")
+public class ProjectXTecnology {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idProject;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tecnology", nullable = false, foreignKey = @ForeignKey(name = "fk_project_tecnology"))
+    private Tecnology tecnology;
+
+    @ManyToOne
+    @JoinColumn(name = "id_project", nullable = false, foreignKey = @ForeignKey(name = "fk_project"))
+    private Project project;
+}
