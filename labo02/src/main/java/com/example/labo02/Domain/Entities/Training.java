614d2ab5b1622f5f5e1190a441ec3423fa5a5f8a
22Cvas <jcvas@outlook.com>
2025-05-08 23:38:04 -0600

    create entities training
    Se crearon las entidades Training, EMployeeXTraining, Assessments y AssessmentsType

---
+++ labo02/src/main/java/com/example/labo02/Domain/Entities/Training.java
@@ -0,0 +1,36 @@
+package com.example.labo02.Domain.Entities;
+
+import com.fasterxml.jackson.databind.DatabindException;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+
+public class Training {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int IdTraining;
+
+    @Column
+    private String topicTitle;
+
+    @Column
+    private Date date;
+
+    @Column
+    private int time;
+
+    @Column
+    private String description;
+
+}
