d012a7b9f3e6d516cbc2bcb96484397478893818
diegohdez33 <00007222@uca.edu.sv>
2025-05-08 19:09:48 -0600

    Add: Area Repository

---
+++ labo02/src/main/java/com/example/labo02/Repository/iAreaRepository.java
@@ -0,0 +1,20 @@
+package com.example.labo02.Repository;
+
+import com.example.labo02.Domain.Entities.Area;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+@Transactional
+public interface iAreaRepository extends iGenericRepository<Area, Integer> {
+    //JPa
+    public Area findByName(String name);
+
+    //Hibrida
+    @Query("SELECT a FROM Area a WHERE a.idArea = :id")
+    public Area findByNameHib(@Param("id_area") Integer id);
+
+    //Nativa
+    @Query(nativeQuery = true, value = "SELECT * FROM area WHERE name = :name")
+    public Area findByNameNative(@Param("name") String name);
+}
