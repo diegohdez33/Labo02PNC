b5a9289bc4dcf7310d22eeddd9bd9c553c1ad13f
22Cvas <jcvas@outlook.com>
2025-05-09 02:43:40 -0600

    insert EmployeeXtraining

---
+++ labo02/src/main/java/com/example/labo02/Domain/Entities/EmployeeXTraining.java
@@ -0,0 +1,30 @@
+package com.example.labo02.Domain.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+
+public class EmployeeXTraining {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int IdEmpleeXTraining;
+
+    @ManyToOne
+    @JoinColumn(name = "IdTraining", nullable = false, foreignKey = @ForeignKey(name = "fk_EmpleeXTraining_Training"))
+    private Training Training;
+
+/*
+    @ManyToOne
+    @JoinColumn(name = "id_employee", nullable = false, foreignKey = @ForeignKey(name = "fk_EmpleeXTraining_Emplee"))
+    private Employee Employee;
+    */
+
+}
